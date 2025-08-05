90a7825dcf5589b68e3a3bf530af9edb0e434022
danielite123 <danielite100@gmail.com>
2025-08-06 00:06:50 +0100

    feat: design the auth ui sucessfully

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -8,6 +8,9 @@ import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import Image from "next/image";
 import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -18,6 +21,7 @@ const authFormSchema = (type: FormType) => {
 };
 
 const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
   const formSchema = authFormSchema(type);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -30,7 +34,18 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    try {
+      if (type === "sign-up") {
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        toast.success("Signed in successfully.");
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
+    }
   }
 
   const isSignIn = type === "sign-in";
@@ -50,9 +65,29 @@ const AuthForm = ({ type }: { type: FormType }) => {
             onSubmit={form.handleSubmit(onSubmit)}
             className="w-full space-y-6 mt-4 form"
           >
-            {!isSignIn && <p>Name</p>}
-            <p>Email</p>
-            <p>Password</p>
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="John Doe"
+              />
+            )}
+            <FormField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="john@example.com"
+              type="email"
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
 
             <Button className="btn" type="submit">
               {isSignIn ? "Sign in" : "Create an Account"}
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -6,18 +6,37 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
 
-const FormField = () => (
-  <FormField
-    control={form.control}
-    name="username"
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
     render={({ field }) => (
       <FormItem>
-        <FormLabel>Username</FormLabel>
+        <FormLabel className="label">{label}</FormLabel>
         <FormControl>
-          <Input placeholder="shadcn" {...field} />
+          <Input
+            className="input"
+            placeholder={placeholder}
+            {...field}
+            type={type}
+          />
         </FormControl>
-        <FormDescription>This is your public display name.</FormDescription>
         <FormMessage />
       </FormItem>
     )}
